f0b54f1d413f3ffe4559d137910a5ae906f67185
yangxue6 <yangxue6@vipkid.com.cn>
2019-09-19 12:03:14 +0800

    init

---
+++ src/test/java/com/sdy/eurekaserver/SdyEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sdy.eurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SdyEurekaServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
